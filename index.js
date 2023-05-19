2e80a95a5d6f467c532905235aa22f66b5e8c223
AhmedRahi <rezarahi71@gmail.com>
2023-05-20 04:21:18 +0600

    single toy find

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
       const result = await toysCollection.deleteOne(query)
       res.send(result)
     })
+
+    app.get('/singletoy/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toysCollection.findOne(query)
+      res.send(result)
+    })
   
 
     // Send a ping to confirm a successful connection
